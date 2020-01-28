a161a50b3f4a071d091fb0851f6a4caa995d2ae8
hhassen <hhtun21@yahoo.fr>
2020-01-28 14:21:57 +0100

    Parametrized Approach without AI

--- hello_gym.py
+++ hello_gym.py
@@ -1,24 +1,55 @@
 import gym
 import numpy as np
+from gym import wrappers
 
 env = gym.make('CartPole-v0')
 
+bestLength = 0
+episode_lengths = []
+
+best_weights = np.zeros(4)
+
+for i in range(100):
+    new_weights = np.random.uniform(-1.0, 1.0, 4)
+    observation = env.reset()
+    done = False
+    cnt = 0
+
+    length = []
+    for j in range(100):
+        while not done:
+            #env.render()
+            cnt += 1
+
+            action = 1 if np.dot(observation, new_weights) > 0 else 0
+
+            observation, reward, done, _ = env.step(action)
+
+            if done:
+                break
+        length.append(cnt)
+    average_length = float(sum(length) / len(length))
+
+    if average_length > bestLength:
+        bestLength = average_length
+        best_weights = new_weights
+    episode_lengths.append(average_length)
+    if i % 10 == 0:
+        print('best length is ', bestLength)
+
 done = False
 cnt = 0
+#env = wrappers.Monitor(env, 'MovieFile2', force=True)
 observation = env.reset()
-print('observation 0 : ', observation)
-
 
 while not done:
-    env.render()
+    # env.render()
     cnt += 1
-
-    action = env.action_space.sample()
-
+    action = 1 if np.dot(observation, best_weights) > 0 else 0
     observation, reward, done, _ = env.step(action)
 
     if done:
         break
 
 env.close()
-print('game lasted : ', cnt, 'moves')
+print('game with best weights lasted : ', cnt, 'moves')
